16ffd9fb37acc1b9290fc06f3633fd883fb2effa
Dimitris-Dedousis <72694765+dimidedou@users.noreply.github.com>
2024-07-02 15:18:38 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -5,24 +5,12 @@ const articles = document.querySelectorAll('.post');
 // Λειτουργία φίλτρου για την αναζήτηση
 searchInput.addEventListener('keyup', function() {
     const query = searchInput.value.toLowerCase();
-    const words = query.trim().split(/\s+/);
-
-    // Δημιουργία κανονικής έκφρασης για κάθε λέξη που εισήχθη
-    const regexps = words.map(word => new RegExp(`\\b${word}\\b`, 'i'));
-
+    
     articles.forEach(article => {
         const title = article.querySelector('h2').textContent.toLowerCase();
         const content = article.querySelector('p').textContent.toLowerCase();
-
-        let matchFound = false;
-        for (let i = 0; i < regexps.length; i++) {
-            if (regexps[i].test(title) || regexps[i].test(content)) {
-                matchFound = true;
-                break;
-            }
-        }
-
-        if (matchFound) {
+        
+        if (title.includes(query) || content.includes(query)) {
             article.style.display = 'block';
         } else {
             article.style.display = 'none';
